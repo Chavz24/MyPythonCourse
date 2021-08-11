b4b2ad91456240f1c9d49e1cd5aaa4b8b6562f76
Chavz24 <miguelchavezh@hotmail.com>
2021-08-11 12:07:30 -0400

    Ejercicio 3

---
+++ D_Automate_the_boring_stuff_with_python/H_Chapter_14/C_Finding_mistakes.py
@@ -0,0 +1,17 @@
+"""Este programa buca un error en un googlesheet y le indica al usuario la celda
+    donde esta el error"""
+
+import ezsheets
+
+ss=ezsheets.Spreadsheet('1jDZEdvSIh4TmZxccyy0ZXrH-ELlrwq8_YYiZrEOB4jg')
+
+sheet=ss[0]
+
+for i,row in enumerate(sheet,start=1):
+    try:
+        if (int(row[0])*int(row[1]))==int(row[2]):
+            continue
+        else:
+            print(f'There is a mistake in cell {ezsheets.convertAddress(3,i)}')
+    except ValueError:
+        continue
