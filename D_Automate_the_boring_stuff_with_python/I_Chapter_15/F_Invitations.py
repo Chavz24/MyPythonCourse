4eaa54ee1b6bf13c8099e19990290d03cee7d508
Chavz24 <miguelchavezh@hotmail.com>
2021-08-12 18:24:06 -0400

    Ejercicio 2

---
+++ D_Automate_the_boring_stuff_with_python/I_Chapter_15/F_Invitations.py
@@ -0,0 +1,44 @@
+
+
+import docx
+from docx.shared import Pt
+from docx.enum.text import WD_BREAK
+
+guest_names=open('FF_Guests.txt','r')
+guest_list=[]
+
+for line in guest_names:
+    guest_list.append(line.strip('\n'))
+
+
+invitations=docx.Document('FF_Custom_invitations.docx')
+paragraph=0
+
+for guest in guest_list:
+    invitations.paragraphs[paragraph].add_run('Nos gustaria contar con la placentera presencia de')
+    invitations.paragraphs[paragraph].runs[0].add_break()
+
+    invitations.paragraphs[paragraph].add_run(f'{guest}')
+    guest_name=invitations.paragraphs[paragraph].runs[1]
+    guest_name.font.name='Calibri'
+    guest_name.font.size= Pt(35)
+    guest_name.font.bold=True
+    guest_name.font.imprint=True
+    guest_name.add_break()
+
+    invitations.paragraphs[paragraph].add_run('en el Boulevard Santiago Mandrades la noche del')
+    invitations.paragraphs[paragraph].runs[2].add_break()
+
+    fecha=invitations.paragraphs[paragraph].add_run('24 de Dic')
+    fecha.font.name='Calibri'
+    fecha.font.size=Pt(25)
+    fecha.add_break()
+
+    #Added this conditional to avoid a blank page at the end
+    if (paragraph+1)<len(guest_list):
+        invitations.paragraphs[paragraph].add_run('a partir de las 9:00 PM').add_break(WD_BREAK.PAGE)
+        invitations.add_paragraph().style='Chavez24'
+        paragraph+=1
+
+
+    invitations.save('FF_Custom_invitations1.docx')
