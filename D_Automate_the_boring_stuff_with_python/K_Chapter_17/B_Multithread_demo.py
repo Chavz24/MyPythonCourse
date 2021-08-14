2e6ba13329c3543f95584ada8707910ad113905b
Chavz24 <miguelchavezh@hotmail.com>
2021-08-14 18:19:19 -0400

    Ejemplo 2

---
+++ D_Automate_the_boring_stuff_with_python/K_Chapter_17/B_Multithread_demo.py
@@ -0,0 +1,20 @@
+import datetime,time,threading
+
+# noche_buena_2021=datetime.datetime(2021,12,24)
+#
+# print(noche_buena_2021>datetime.datetime.now())
+#
+# while noche_buena_2021>datetime.datetime.now():
+#     time.sleep(10)
+#
+#     print('hola')
+
+print('Cominezo del programa..')
+
+def pausa():
+    time.sleep(2)
+    print('Se acabo la pausa')
+
+thread_obj=threading.Thread(target=pausa)
+thread_obj.start()
+print('Final del programa')
