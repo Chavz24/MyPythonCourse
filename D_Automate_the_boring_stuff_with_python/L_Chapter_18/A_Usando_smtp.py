feb09e672f79899d173449a14b311a5598d78b6f
Chavz24 <miguelchavezh@hotmail.com>
2021-08-17 17:59:00 -0400

    Ejemplo

--- D_Automate_the_boring_stuff_with_python/L_Chapter_18/A_Usando_smtp.py
+++ D_Automate_the_boring_stuff_with_python/L_Chapter_18/A_Usando_smtp.py
@@ -1,14 +1,12 @@
 import smtplib
 
-smtp_obj= smtplib.SMTP('domino',0)#esto es el nombre del dominio del servidor y el numero del puerto
+smtp_obj= smtplib.SMTP('smtp.gmail.com',587)#esto es el nombre del dominio del servidor y el numero del puerto
 smtp_obj.ehlo()
 smtp_obj.starttls()#encriptando la coneccion.
 smtp_obj.login('mi_correo','mi_contrasenia')
 smtp_obj.sendmail('mi_correo',['Correo_destino_1','Correo_destino_2'],
                   'Subject:Prueba.\nEste es otro correo de prueba usando SMTP')
 smtp_obj.quit()#desconectando del servidor.
-
-
 
 
 
